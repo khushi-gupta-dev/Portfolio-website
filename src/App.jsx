91fb05c97d9410420e67fb1269619fbec1f18a19
khushi-gupta-dev <khushi.gupta28.dev@gmail.com>
2026-05-28 21:56:50 +0530

    added radial Gradient bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/Hero";
-
+import About from "./components/sections/About";
 const App = () => {
   return ( 
     <div className="min-h-screen bg-black pb-[100vh]">
